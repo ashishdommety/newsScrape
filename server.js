abfe50a8d05bb969b2e550ed2eff0a4e14fc0925
unknown <ashishuiux@gmail.com>
2017-09-08 12:06:39 -0700

    cleaning up the server file and adding mongoose functionality

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-//our Dependencies
+//Dependencies
 const express = require("express");
 const bodyParser = require("body-parser");
 const methodOverride = require("method-override");
@@ -7,23 +7,39 @@ const request = require("request");
 const logger = require("morgan");
 const mongoose = require("mongoose");
 
+// Requiring our Note and Article models
+var Note = require("./models/Note.js");
+var Article = require("./models/Article.js");
+
+// Set mongoose to leverage built in JavaScript ES6 Promises
+mongoose.Promise = Promise;
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.static("assets"));
 
-app.use(bodyParser.json());
+app.use(logger("dev"));
 app.use(bodyParser.urlencoded({
   extended: true
 }));
-app.use(bodyParser.text());
-app.use(bodyParser.json({
-  type: "application/vnd.api+json"
-}));
 
-// Serve static content for the app from the "public" directory in the application directory.
-app.use(bodyParser.urlencoded({ extended: true }));
+// app.use(bodyParser.text());
+// app.use(bodyParser.json({
+//   type: "application/vnd.api+json"
+// }));
+mongoose.connect("mongodb://localhost/newscrape");
+var db = mongoose.connection;
+
+// Show any mongoose errors
+db.on("error", function(error) {
+  console.log("Mongoose Error: ", error);
+});
+
+// Once logged in to the db through mongoose, log a success message
+db.once("open", function() {
+  console.log("Mongoose connection successful.");
+});
 
 // Override with POST having ?_method=DELETE
 app.use(methodOverride("_method"));
